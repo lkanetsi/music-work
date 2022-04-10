4427d3ea27afed0224da045ab7f1e2fbb838bc67
hlony <lakanetsi@gmail.com>
2022-04-10 10:55:23 +0200

    added Musical model

--- backend/musicwork/app/models.py
+++ backend/musicwork/app/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 from simple_history.models import HistoricalRecords
 
+
 class MusicalWork(models.Model):
-    """Model for musical compostion metadata"""
+
+    """ musical compostion metadata records"""
+
     title = models.CharField(max_length=255)
     contributors = models.CharField(max_length=255)
-    iswc = models
+    iswc = models.CharField(max_length=30)
     history = HistoricalRecords()
-    
+    created_at = models.DateTimeField(auto_now_add=True,)
+
     class Meta:
         ordering = ('title',)
 
     def __str__(self):
-        return self.title 
+        return self.title
+
+
