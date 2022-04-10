e1c147f32e7067c43e0072b48f08590caec20e72
hlony <lakanetsi@gmail.com>
2022-04-10 10:46:49 +0200

    added simple history package and musical model

--- backend/musicwork/app/models.py
+++ backend/musicwork/app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from simple_history.models import HistoricalRecords
 
-# Create your models here.
+class MusicalWork(models.Model):
+    """Model for musical compostion metadata"""
+    title = models.CharField(max_length=255)
+    contributors = models.CharField(max_length=255)
+    iswc = models
+    history = HistoricalRecords()
+    
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title 
